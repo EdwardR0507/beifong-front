96c094ae9137c05b0f3ec29da42e178d88b947ba
EdwardR0507 <ramosedward21@gmail.com>
2021-12-11 22:05:46 -0500

    feat: Text to Speach

---
+++ hooks/useTextToSpeech.js
@@ -0,0 +1,12 @@
+import { useState } from "react"
+
+export const useTextToSpeech = () => {
+  const [isTextToSpeech, setIsTextToSpeech] = useState(false)
+
+  const handleSpeak = (text) => {
+    const speech = new SpeechSynthesisUtterance(text)
+    !isTextToSpeech ? speechSynthesis.speak(speech) : speechSynthesis.cancel()
+    setIsTextToSpeech(!isTextToSpeech)
+  }
+  return { isTextToSpeech, handleSpeak }
+}
