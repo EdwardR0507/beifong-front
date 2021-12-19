64c665937840a226ef6223172f41d09fc91a1878
Railly <railly_2009@hotmail.com>
2021-12-18 20:55:18 -0500

    Add extra validations to clinic register

--- schemas/clinica/registro.js
+++ schemas/clinica/registro.js
@@ -1,12 +1,19 @@
 import * as yup from "yup"
 
 export const clinicRegisterSchema = yup.object().shape({
-  name: yup.string().required("El nombre es requerido"),
+  name: yup
+    .string()
+    .required("El nombre es requerido")
+    .max(70, "El nombre no puede tener más de 70 caracteres")
+    .matches(/^[a-zA-Z\s]*$/, "El nombre solo puede contener letras"),
   telephone: yup
     .string()
     .required("El teléfono es requerido")
     .matches(/^9\d{8}$/, "El teléfono debe comenzar con 9 y tener 9 dígitos"),
-  direction: yup.string().required("La dirección es requerida"),
+  direction: yup
+    .string()
+    .required("La dirección es requerida")
+    .max(70, "La dirección no puede tener más de 70 caracteres"),
   email: yup
     .string()
     .email("El email no es válido")
@@ -17,5 +24,6 @@ export const clinicRegisterSchema = yup.object().shape({
     .matches(
       /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/,
       "La contraseña debe contener al menos 1 minúscula, 1 mayúscula, 1 número y debe tener al menos 8 caracteres"
-    ),
+    )
+    .max(30, "La contraseña no puede tener más de 30 caracteres"),
 })
