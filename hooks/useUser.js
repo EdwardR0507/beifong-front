70cbc150e670024fdf4268ab13598225e11e65f2
Railly <railly_2009@hotmail.com>
2022-01-08 21:05:44 -0500

    fix remove useUser

--- hooks/useUser.js
+++ hooks/useUser.js
@@ -1,9 +1,9 @@
 import { UserContext } from "context/UserContext"
-import { useRouter } from "next/router"
+// import { useRouter } from "next/router"
 import { useContext, useEffect } from "react"
 
 export default function useUser(role) {
-  const router = useRouter()
+  // const router = useRouter()
   const {
     user,
     setUser,
@@ -14,14 +14,14 @@ export default function useUser(role) {
   } = useContext(UserContext)
 
   useEffect(() => {
-    const user = window.localStorage.getItem("user")
+    // const user = window.localStorage.getItem("user")
     const accessibility = window.localStorage.getItem("accessibility")
-    if (!user) {
-      router.push(`/${role}/login`)
-    } else {
-      setUser(JSON.parse(user))
-      setAccessibility(JSON.parse(accessibility))
-    }
+    // if (!user) {
+    //   router.push(`/${role}/login`)
+    // } else {
+    setUser(JSON.parse(user))
+    setAccessibility(JSON.parse(accessibility))
+    // }
   }, [reloadUser])
 
   return { user, accessibility, reload: () => setReloadUser(!reloadUser) }
